02c57bba3c7a43e80e4d522316254360a3d7861b
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-07-23 21:43:03 +0200

    Adding some real assertion in test cases

--- search_test.go
+++ search_test.go
@@ -1,26 +1,39 @@
 package solr
 
 import "testing"
+
 import "fmt"
 
-func TestSolrQuery(t *testing.T) {
-	q := Query{
-		params: QueryParams{
-			"q": "{!type=dismax qf=myfield v='solr rocks'}",
-		},
-	}
-
-	//q := NewQuery()
-
-	q.params["bf"] = "something"
-
+func TestSolrQueryAddParam(t *testing.T) {
+	
+	q := NewQuery()
 	q.AddParam("qf", "some qf")
-	q.AddParam("sbrm", "should be removed")
-	q.RemoveParam("sbrm")
+		
+	if q.String() != "qf=some+qf" {
+		t.Errorf("Expected to be: 'some qf'")
+	}
+	
 	fmt.Println(q.String())
 }
 
-func TestSolrSearch(t *testing.T) {
-	//s := new(Search)
-	//s.conn = &Connection
+func TestSolrQueryMultiple(t *testing.T) {
+	q := NewQuery()
+	q.AddParam("testing", "test")
+	s := NewSearch(q)
+	q2 := NewQuery()
+	q2.AddParam("testing", "testing 2")
+	s.AddQuery(q2)
+	if s.QueryString() != "testing=test&testing=testing+2" {
+		t.Errorf("Expected to be: 'testing=test&testing=testing+2'")
+	}
+}
+
+func TestSolrQueryRemoveParam(t *testing.T) {
+	q := NewQuery()
+	q.AddParam("testing", "test")
+	q.AddParam("testing2", "testing 2")
+	q.RemoveParam("testing2")
+	if q.String() != "testing=test" {
+		t.Errorf("Expected to be: 'testing=test'")
+	}
 }
