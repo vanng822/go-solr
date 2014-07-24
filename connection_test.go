dc3b33e764b7f82fbb98909f966661f0fd389028
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-07-24 23:26:44 +0200

    Try out json2bytes as well

--- connection_test.go
+++ connection_test.go
@@ -15,7 +15,10 @@ func TestConnection(t *testing.T) {
 func TestBytes2Json(t *testing.T) {
 	data := []byte(`{"t":"s","two":2,"obj":{"c":"b","j":"F"},"a":[1,2,3]}`)
 	d, _ := bytes2Json(&data)
+	PrintMapInterface(d)
+}
 
+func PrintMapInterface(d map[string]interface{}) {
 	for k, v := range d {
 		switch vv := v.(type) {
 		case string:
@@ -25,7 +28,12 @@ func TestBytes2Json(t *testing.T) {
 		case float64:
 			fmt.Println(k, "is float", vv)
 		case map[string]interface{}:
+			fmt.Println(k, "type is map[string]interface{}")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
 		case []interface{}:
+			fmt.Println(k, "type is []interface{}")
 			for i, u := range vv {
 				fmt.Println(i, u)
 			}
@@ -33,5 +41,22 @@ func TestBytes2Json(t *testing.T) {
 			fmt.Println(k, "is of a type I don't know how to handle", vv)
 		}
 	}
+}
 
+func TestJson2Bytes(t *testing.T) {
+
+	test_json := map[string]interface{}{
+		"t":   "s",
+		"two": 2,
+		"obj": map[string]interface{}{"c": "b", "j": "F"},
+		"a":   []interface{}{1, 2, 3},
+	}
+
+	b, err := json2Bytes(test_json)
+	if err != nil {
+		fmt.Println(err)
+	}
+	d, _ := bytes2Json(b)
+
+	PrintMapInterface(d)
 }
