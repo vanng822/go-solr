dbc26f7e85c420f5836a911c5b31a12650849b9e
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-07-29 20:19:30 +0200

    Adding test for response with facet_counts

--- solr_test.go
+++ solr_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 func TestSolrDocument(t *testing.T) {
-	d := Document{"id": "test_id","title": "test title"}
+	d := Document{"id": "test_id", "title": "test title"}
 	if d.Has("id") == false {
 		t.Errorf("Has id expected to be true")
 	}
-	
+
 	if d.Has("not_exist") == true {
 		t.Errorf("Has not_exist expected to be false")
 	}
-	
+
 	if d.Get("title").(string) != "test title" {
 		t.Errorf("title expected to have value 'test title'")
 	}
-	
+
 	d.Set("new_title", "new title")
 	if d.Get("new_title").(string) != "new title" {
 		t.Errorf("new_title expected to have value 'new title'")
@@ -33,40 +33,39 @@ func TestSolrSuccessSelect(t *testing.T) {
 	if err != nil {
 		t.Errorf("Can not instance a new solr interface, err: %s", err)
 	}
-	
+
 	q := NewQuery()
 	q.AddParam("q", "*:*")
 	s := si.Search(q)
-	
+
 	res, err := s.Result(nil)
 
 	if err != nil {
 		t.Errorf("cannot seach %s", err)
 	}
-	
+
 	if res.status != 0 {
 		t.Errorf("Status expected to be 0")
 	}
-	
+
 	if res.results.numFound != 1 {
 		t.Errorf("results.numFound expected to be 1")
 	}
-	
+
 	if res.results.start != 0 {
 		t.Errorf("results.start expected to be 0")
 	}
-	
+
 	if len(res.results.docs) != 1 {
 		t.Errorf("len of .docs should be 1")
 	}
-	
+
 	if res.results.docs[0].Get("id").(string) != "change.me" {
 		t.Errorf("id of first document should be change.me")
 	}
-	
+
 	fmt.Println(" ")
 }
-
 
 func TestSolrFailSelect(t *testing.T) {
 	si, err := NewSolrInterface("http://127.0.0.1:12345/fail")
@@ -74,33 +73,96 @@ func TestSolrFailSelect(t *testing.T) {
 	if err != nil {
 		t.Errorf("Can not instance a new solr interface, err: %s", err)
 	}
-	
+
 	q := NewQuery()
 	q.AddParam("q", "*:*")
 	s := si.Search(q)
-	
+
 	parser := new(StandardResultParser)
 	res, err := s.Result(parser)
-	
+
 	if err != nil {
 		t.Errorf("cannot seach %s", err)
 	}
-	
+
 	if res.status != 400 {
 		t.Errorf("Status expected to be 400")
 	}
-	
+
 	if res.results.numFound != 0 {
 		t.Errorf("results.numFound expected to be 0")
 	}
-	
+
 	if res.results.start != 0 {
 		t.Errorf("results.start expected to be 0")
 	}
-	
+
 	if len(res.results.docs) != 0 {
 		t.Errorf("len of .docs should be 0")
 	}
-	
+
 	fmt.Println(" ")
+}
+
+func TestSolrFacetSelect(t *testing.T) {
+	si, err := NewSolrInterface("http://127.0.0.1:12345/facet_counts")
+
+	if err != nil {
+		t.Errorf("Can not instance a new solr interface, err: %s", err)
+	}
+
+	q := NewQuery()
+	q.AddParam("q", "*:*")
+	q.AddParam("facet", "true")
+	q.AddParam("facet.field", "id")
+
+	s := si.Search(q)
+	fmt.Println(s.QueryString())
+	parser := new(StandardResultParser)
+	res, err := s.Result(parser)
+
+	if err != nil {
+		t.Errorf("cannot seach %s", err)
+	}
+
+	if res.status != 0 {
+		t.Errorf("Status expected to be 0 but got %d", res.status)
+	}
+
+	if res.results.numFound != 4 {
+		t.Errorf("results.numFound expected to be 4 but got %d", res.results.numFound)
+	}
+
+	if res.results.start != 0 {
+		t.Errorf("results.start expected to be 0 but got %d", res.results.start)
+	}
+
+	if len(res.results.docs) != 4 {
+		t.Errorf("len of .docs should be 4 but got %d", len(res.results.docs))
+	}
+
+	third_doc := res.results.docs[2]
+
+	if third_doc.Get("id") != "change.me3" {
+		t.Errorf("id of third document expected to be 'change.me3' but got '%s'", third_doc.Get("id"))
+	}
+
+	if _, ok := res.facet_counts["facet_fields"]; ok == false {
+		t.Errorf("results.facet_counts.facet_fields expected")
+		return
+	}
+
+	facet_fields := res.facet_counts["facet_fields"].(map[string]interface{})
+	id, ok := facet_fields["id"]
+
+	if ok == false {
+		t.Errorf("results.facet_counts.facet_fields.id expected")
+		return
+	}
+
+	id_len := len(id.([]interface{}))
+
+	if id_len != 6 {
+		t.Errorf("results.facet_counts.facet_fields.id.len expected be 6 but got %d", id_len)
+	}
 }
--- test_mock.go
+++ test_mock.go
@@ -23,6 +23,46 @@ func mockSuccessSelect(w http.ResponseWriter, req *http.Request) {
 		  }}`)
 }
 
+func mockSuccessSelectFacet(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, `{
+				  "responseHeader":{
+				    "status":0,
+				    "QTime":10,
+				    "params":{
+				      "facet":"true",
+				      "indent":"true",
+				      "q":"*:*",
+				      "facet.field":"id",
+				      "wt":"json"}},
+				  "response":{"numFound":4,"start":0,"docs":[
+				      {
+				        "id":"change.me",
+				        "title":["change.me"],
+				        "_version_":1474893319511212032},
+				      {
+				        "id":"change.me2",
+				        "title":["change.me2"],
+				        "_version_":1474893328448225280},
+				      {
+				        "id":"change.me3",
+				        "title":["change.me3"],
+				        "_version_":1474893336208736256},
+				      {
+				        "id":"change.me2",
+				        "title":["change.me22"],
+				        "_version_":1474893362047746048}]
+				  },
+				  "facet_counts":{
+				    "facet_queries":{},
+				    "facet_fields":{
+				      "id":[
+				        "change.me2",2,
+				        "change.me",1,
+				        "change.me3",1]},
+				    "facet_dates":{},
+				    "facet_ranges":{}}}`)
+}
+
 func mockFailSelect(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(400)
 	io.WriteString(w, `{
@@ -41,6 +81,7 @@ func mockFailSelect(w http.ResponseWriter, req *http.Request) {
 func mockStartServer() {
 	http.HandleFunc("/success/select/", mockSuccessSelect)
 	http.HandleFunc("/fail/select/", mockFailSelect)
+	http.HandleFunc("/facet_counts/select/", mockSuccessSelectFacet)
 
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
